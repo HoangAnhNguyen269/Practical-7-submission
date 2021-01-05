be37f5e7e26b151359c3cf752fa30ae2f0af9c9e
Nguyễn Hoàng <vodanh269210@gmail.com>
2021-01-05 09:32:58 +0000

    Initial commit

---
+++ myjavascript.js
@@ -0,0 +1,41 @@
+/*let message = "Hello!";
+alert(message);
+message = "What is your name?";
+let userName = prompt(message);
+alert("You said: " + userName);
+if (userName.length < 5) {
+ userName = prompt("Please enter a user name that has more than one letter.");
+ alert(userName);
+}
+else{
+ alert("Your username was accepted on the first try!");
+};
+while (userName.length <= 0){
+ userName = prompt("Please enter a user name that has at least one letter.");
+ }
+ if (userName.length < 2){
+ alert("I see you are using an initial");
+ }
+ else{
+ alert("Your userName is now updated");
+ };
+  let userAge = prompt("Enter your age");
+ while (userAge <=0){
+   userAge = prompt("Enter a valid age");
+ };*/
+function showMessage(prompt) {
+ alert(prompt);
+ };
+function oneToThree(){
+   for (let i = 0; i < 10; i++) {
+document.write(i + ": " + i*3 + "<br />");
+};
+var sum = 0;
+for (let i = 0; i < a.length; i++) {
+sum += a[i];
+};      
+html = "";
+for (let i of custOrder) {
+html += "<li>" + i + "</li>";
+}
+ };
